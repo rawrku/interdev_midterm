3175488da5e213da41c65178f1ba0d24a1c72333
-AKIRA-\ruari <ruarisroot@icloud.com>
2023-10-25 05:00:05 -0400

    ITS DONE

--- Assets/Game Scene/Scripts/PikaHand.cs
+++ Assets/Game Scene/Scripts/PikaHand.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PikaHand : MonoBehaviour
 {
     public Texture2D customCursor;
-    public Vector2 cursorHotspot = Vector2.zero;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot;
 
     void Start()
     {
-        Cursor.SetCursor(customCursor, cursorHotspot, CursorMode.Auto);
+        hotSpot = new Vector2(customCursor.width / 2, 0);
+        Cursor.SetCursor(customCursor, hotSpot, cursorMode);
     }
 }
